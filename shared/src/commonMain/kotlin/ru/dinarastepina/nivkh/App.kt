3716da278491de12c350320c450bf16414f21c95
dinarastepina <setare497@gmail.com>
2023-12-11 16:10:05 -0500

    added a theme

--- shared/src/commonMain/kotlin/ru/dinarastepina/nivkh/App.kt
+++ shared/src/commonMain/kotlin/ru/dinarastepina/nivkh/App.kt
@@ -3,7 +3,7 @@ package ru.dinarastepina.nivkh
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import ru.dinarastepina.nivkh.presentation.screens.home.HomeScreen
-import ru.dinarastepina.nivkh.presentation.ui.MyApplicationTheme
+import ru.dinarastepina.nivkh.presentation.ui.theme.MyApplicationTheme
 
 @Composable
 fun App() {
