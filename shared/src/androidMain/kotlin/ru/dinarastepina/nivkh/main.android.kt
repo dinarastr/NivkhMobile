1edb925f0993b5bc284c82f004cf0ceb4ad53ae9
dinarastepina <setare497@gmail.com>
2023-12-12 12:18:28 -0500

    added image caching for android app

--- shared/src/androidMain/kotlin/ru/dinarastepina/nivkh/main.android.kt
+++ shared/src/androidMain/kotlin/ru/dinarastepina/nivkh/main.android.kt
@@ -1,9 +1,36 @@
 package ru.dinarastepina.nivkh
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
+import com.seiko.imageloader.ImageLoader
+import com.seiko.imageloader.LocalImageLoader
+import com.seiko.imageloader.cache.memory.maxSizePercent
+import com.seiko.imageloader.component.setupDefaultComponents
+import okio.Path.Companion.toOkioPath
 
 
 @Composable
 fun MainView() {
-    App()
+    val context = LocalContext.current
+
+    CompositionLocalProvider(
+        LocalImageLoader provides ImageLoader {
+            components {
+                setupDefaultComponents(context)
+            }
+            interceptor {
+                //defaultImageResultMemoryCache()
+                memoryCacheConfig {
+                    maxSizePercent(context, 0.25)
+                }
+                diskCacheConfig {
+                    directory(context.cacheDir.resolve("image_cache").toOkioPath())
+                    maxSizeBytes(512L * 1024 * 1024) // 512MB
+                }
+            }
+        },
+    ) {
+        App()
+    }
 }
